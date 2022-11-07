ca7fee6f65fd1dad7d6e6140895e94896717d696
adiprase <116736741+adiprase@users.noreply.github.com>
2022-11-07 12:02:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-/* Delete ?m=1 */ var uri = window.location.toString();if (uri.indexOf("?m=1","?m=1") > 0) {var clean_uri = uri.substring(0, uri.indexOf("?m=1"));window.history.replaceState({}, document.title, clean_uri);}
-
 /* Header Hide Down */ var prevScrollpos = window.pageYOffset;
   window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
